d113086f74f7fe036caf03e0f9091b5f721b85d9
Dolandre <100500283+Dolandre@users.noreply.github.com>
2023-10-11 14:25:11 +0800

    Add files via upload

---
+++ Day3.java
@@ -0,0 +1,28 @@
+package dolandre_Febrianto;
+
+public class Day3 {
+
+    public static void main(String[] args) {
+        //field
+        int angka1 = 10;
+        int angka2 = 2;
+
+        //method
+        int penjumlahan = angka1 + angka2;
+        System.out.println("Penjumlahan: " + penjumlahan);
+
+        int pengurangan = angka1 - angka2;
+        System.out.println("Pengurangan: " + pengurangan);
+
+        int perkalian = angka1 * angka2;
+        System.out.println("Perkalian: " + perkalian);
+
+        int pembagian = angka1 / angka2;
+        System.out.println("Pembagian: " + pembagian);
+
+        int sisa_bagi = angka1 % angka2;
+        System.out.println("Sisa Bagi: " + sisa_bagi);
+                     
+    }
+
+}
