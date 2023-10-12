16ba96097751fdf228c0e53cd95d164e722a520a
Dolandre <100500283+Dolandre@users.noreply.github.com>
2023-10-12 20:34:48 +0800

    Add files via upload

---
+++ Day4.java
@@ -0,0 +1,15 @@
+package dolandre_Febrianto;
+
+import java.util.Scanner;
+
+public class Day4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        //perulangan
+        for(int i = 1; i <= 10; i++){
+            System.out.println(i);
+        }
+        scanner.close();
+    }
+    
+}
