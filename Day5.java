4c4a36c3f35c8e129f4d0e2a4027385e483f5b6a
Dolandre <100500283+Dolandre@users.noreply.github.com>
2023-10-13 20:40:22 +0800

    Add files via upload

---
+++ Day5.java
@@ -0,0 +1,12 @@
+package dolandre_Febrianto;
+
+public class Day5 {
+    public static void main(String[] args) {
+        int i = 0;
+        while (i <= 5 ){
+            System.out.println("Angka: "+ i);
+            i++;
+        }
+    }
+    
+}
