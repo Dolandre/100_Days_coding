f126746b6ef51ef1ef60b92da44496c58521376a
Dolandre <100500283+Dolandre@users.noreply.github.com>
2023-10-16 22:41:42 +0800

    Add files via upload

---
+++ Day8_Menghitung_Luas_Persegi.java
@@ -0,0 +1,12 @@
+package dolandre_Febrianto;
+
+public class Day8_Menghitung_Luas_Persegi {
+    public static void main(String[] args) {
+        int sisi = 5; 
+        int luas = sisi*sisi; //Rumus Menghitung Luas Persegi
+        
+        System.out.println("Sisi = "+sisi); //Digunakan untuk menampilkan nilai sisi
+        System.out.println("Luas Persegi adalah "+luas); // Digunakan untuk menampilkan Luas Persegi
+    }
+    
+}
