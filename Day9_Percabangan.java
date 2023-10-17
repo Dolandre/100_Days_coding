8a7531d3027d7aeb49090744146fd6813093048a
Dolandre <100500283+Dolandre@users.noreply.github.com>
2023-10-17 20:28:33 +0800

    Add files via upload

---
+++ Day9_Percabangan.java
@@ -0,0 +1,16 @@
+package dolandre_Febrianto;
+
+public class Day9_Percabangan {
+    public static void main(String[] args) {
+        int Angka = 5;
+        
+        if (Angka > 0) {
+            System.out.println("Angka Ini Adalah Positif");
+        }else if (Angka < 0){
+            System.out.println("Angka Ini Adalah Negatif");
+        }else {
+            System.out.println("Angka Ini Adalah Nol");
+        }
+    }
+    
+}
