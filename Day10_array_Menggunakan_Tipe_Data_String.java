baaac700058b586aecc5f462e34aa8ec46ee59be
Dolandre <100500283+Dolandre@users.noreply.github.com>
2023-10-18 22:16:50 +0800

    Add files via upload

---
+++ Day10_array_Menggunakan_Tipe_Data_String.java
@@ -0,0 +1,14 @@
+package dolandre_Febrianto;
+
+import java.util.Arrays;
+
+public class Day10_array_Menggunakan_Tipe_Data_String {
+
+    public static void main(String[] args) {
+        String[] alatTulis = {"Pulpen", "Buku", "Pensil", "Penghapus", "Penggaris"};
+
+        // mencetak menggunakan class Arrays
+        System.out.println(Arrays.toString(alatTulis));
+    }
+    
+}
