2ec692909dfc2a8cec7e5749af7ad5372d9ce33c
Dolandre <100500283+Dolandre@users.noreply.github.com>
2023-10-19 23:13:29 +0800

    Add files via upload

---
+++ Day11_array_menggunakan_looping_each.java
@@ -0,0 +1,15 @@
+package dolandre_Febrianto;
+
+public class Day11_array_menggunakan_looping_each {
+
+    public static void main(String[] args) {
+        String[] alatTulis = {"Pulpen", "Buku", "Pensil", "Penghapus", "Penggaris"};
+
+        // mencetak menggunakan looping for each 
+        for (String a : alatTulis) {
+            System.out.println(a + ";");
+        }
+        System.out.println();
+    }
+
+}
