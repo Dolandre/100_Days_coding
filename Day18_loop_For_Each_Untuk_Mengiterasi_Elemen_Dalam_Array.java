4e6f8aecf5e73c499f0217e70b54090339217bab
Dolandre <100500283+Dolandre@users.noreply.github.com>
2023-10-26 21:05:41 +0800

    Add files via upload

---
+++ Day18_loop_For_Each_Untuk_Mengiterasi_Elemen_Dalam_Array.java
@@ -0,0 +1,12 @@
+package dolandre_Febrianto;
+
+public class Day18_loop_For_Each_Untuk_Mengiterasi_Elemen_Dalam_Array {
+
+    public static void main(String[] args) {
+        
+        String[] names = {"Dolan", "Aniss", "Yhunus", "Riel"};
+        for (String name : names) {
+            System.out.println("Nama: " + name);
+        }
+    }
+}
