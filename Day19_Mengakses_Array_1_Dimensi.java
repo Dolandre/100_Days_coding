84775dfd34f18fca0d432bed83732d06410c2090
Dolandre <100500283+Dolandre@users.noreply.github.com>
2023-10-27 22:46:41 +0800

    Add files via upload

---
+++ Day19_Mengakses_Array_1_Dimensi.java
@@ -0,0 +1,15 @@
+package dolandre_Febrianto;
+
+public class Day19_Mengakses_Array_1_Dimensi {
+
+    public static void main(String[] args) {
+        // membuat array 1 dimensi
+        int[] dolanArray = {1, 2, 3, 4, 5};
+
+        //mengakses elem array
+        System.out.println(dolanArray[0]);
+        System.out.println(dolanArray[2]);
+
+    }
+
+}
