bb8c5b53164d7114aa3a36c0aa4ee8da1e8b8bcb
Dolandre <100500283+Dolandre@users.noreply.github.com>
2023-10-29 22:20:07 +0800

    Add files via upload

---
+++ Day21_Menghitung_Panjang_Array.java
@@ -0,0 +1,19 @@
+package dolandre_Febrianto;
+
+import java.util.Arrays;
+
+public class Day21_Menghitung_Panjang_Array {
+
+    public static void main(String[] args) {
+        // Membuat array 1 dimensi
+        int[] dolanArray = {1, 2, 3, 4, 5, 6, 7, 10};
+
+        System.out.println(Arrays.toString(dolanArray));
+        System.out.println("------------------------------------");
+
+        //Menghitung Panjang Array
+        int length = dolanArray.length;
+        System.out.println(length);
+    }
+
+}
