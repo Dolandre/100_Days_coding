06852ec50d375143cae4d962df2eb6a41f4f70f1
Dolandre <100500283+Dolandre@users.noreply.github.com>
2023-10-30 23:34:23 +0800

    Add files via upload

---
+++ Day22.java
@@ -0,0 +1,10 @@
+package day22;
+public class Day22 {
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+        boolean hasil = a&&b;
+        System.out.println("hasil dari: " + hasil);
+    }
+    
+}
